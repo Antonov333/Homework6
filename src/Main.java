751c8c62ce94fdd394ec3b422c1ab73277d2d644
Antonov333 <antonovsergei333@yahoo.com>
2023-03-01 16:13:47 +0300

    Commit to Homework6 branch

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+// Just started with Homework #6 dedicated to Java statements for cycles and loops
 public class Main {
     public static void main(String[] args) {
         System.out.println("ДОМАШНЕЕ ЗАДАНИЕ №6: ЦИКЛЫ");
